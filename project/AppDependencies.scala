ae92675d40667a5d2d4e3871b566e163fe80159b
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-04-04 17:09:39 +0100

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,16 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-play-25"          % "4.10.0",
-    "uk.gov.hmrc"             %% "govuk-template"             % "5.30.0-play-25",
+    "uk.gov.hmrc"             %% "govuk-template"             % "5.31.0-play-25",
     "uk.gov.hmrc"             %% "play-ui"                    % "7.38.0-play-25"
   )
 
   val scope = "test, it"
 
   val test = Seq(
-    "com.github.tomakehurst"  %  "wiremock"                 % "2.21.0"        % scope,
+    "com.github.tomakehurst"  %  "wiremock"                 % "2.22.0"        % scope,
     "com.typesafe.play"       %% "play-test"                % current         % scope,
-    "org.mockito"             %  "mockito-core"             % "2.25.0"        % scope,
+    "org.mockito"             %  "mockito-core"             % "2.26.0"        % scope,
     "org.pegdown"             %  "pegdown"                  % "1.6.0"         % scope,
     "org.scalatest"           %% "scalatest"                % "3.0.4"         % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"         % scope,
